c40e56b94981c6d6532d1e71c0b2d21489919efe
Pratikshatippe <54614091+Pratikshatippe@users.noreply.github.com>
2019-11-25 10:45:14 +0530

    Added homepage

---
+++ client/src/component/Homepage/homepage.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export class homepage extends React.Children{
+    constructor(props){
+        super(props);
+    }
+
+    render(){
+        return(
+            <div className="base-container">
+                <div className="">
+            </div>
+        )
+    }
+}
