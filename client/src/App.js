a2762ccba103abc38135aacbac84db69134ee66b
Pratikshatippe <54614091+Pratikshatippe@users.noreply.github.com>
2019-11-25 16:26:52 +0530

    try router

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,21 @@
-import React from 'react';
+import React, {Component} from 'react';
 // import logo from './logo.svg';
 import './App.css';
 import Homepage from './component/Homepage/homepage';
+import Log from './component/LoginPage/login';
+import { BrowserRouter as Router, Route} from 'react-router-dom';
 
-function App() {
-  return (
-    <div className="App">
-     <Homepage/>
-    </div>
-  );
+class App extends Component {
+  render(){
+    return (
+      <Router>
+      <div>
+        <Route path="/" component={Homepage}/>
+        <Route  path="/login" component={Log}/>
+        </div>
+      </Router>
+    );
+  }
 }
 
 export default App;
