0b8d775ffac3f3d720e6dc46ff829ecc1a4c0fbf
Pratikshatippe <54614091+Pratikshatippe@users.noreply.github.com>
2019-11-26 12:29:03 +0530

    using switch

--- client/src/App.js
+++ client/src/App.js
@@ -2,21 +2,25 @@ import React, {Component} from 'react';
 // import logo from './logo.svg';
 import './App.css';
 // import Homepage from './component/Homepage/homepage';
-// <Route path="/" component={Homepage} exact/>
-// import Log from './component/LoginPage/login';
-// <Route  path="/login" component={Log}/>
-// <Route  path="/disease" component={Diseases}/>
+// <Route path="/" component={Homepage}/>
+import Log from './component/LoginPage/login';
 // import Diseases from './component/Disease/diseases';
-import Rooms from './component/Rooms/room';
+// import Rooms from './component/Rooms/room';
+// <Route  path="/disease" component={Diseases}/>
+// <Route  path="/rooms" component={Rooms}/>
+import Register from './component/Register/register'
 import { BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 
 class App extends Component {
   render(){
     return (
       <Router>
-        <Switch>
-        <Route  path="/rooms" component={Rooms}/>
-        </Switch>
+        <div className="App">
+          <Switch>
+            <Route  path="/login" component={Log}/>
+            <Route  path="/register" component={Register}/>
+          </Switch>
+        </div>
       </Router>
     );
   }
