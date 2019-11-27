8dd6dc76646019e04b1b51aa877745af7bbee636
Pratikshatippe <54614091+Pratikshatippe@users.noreply.github.com>
2019-11-27 15:48:17 +0530

    updated

--- server/model/register.js
+++ server/model/register.js
@@ -1,16 +1,16 @@
 const mongoose = require('mongoose');
 
-const UserInfoSchema = new.mongoose.Schema({
+const UserInfoSchema = new mongoose.Schema({
     username:{
         type:String,
         required: true,
     },
     password:{
-        type:any,
+        type:String,
         required:true
     },
     confirmPassword:{
-        type:any,
+        type:String,
         required:true
     }
 });
