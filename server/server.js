31d24135882fdc4f4a6a5dea7ffe90c92740bbb4
Pratikshatippe <54614091+Pratikshatippe@users.noreply.github.com>
2019-11-28 10:12:45 +0530

    backend code

---
+++ server/server.js
@@ -0,0 +1,26 @@
+const express =require('express');
+const uri = "mongodb+srv://pratiksha:rBBM2fscP3UQnk4H@cluster0-8sqx5.mongodb.net/Servicer?retryWrites=true&w=majority";
+const app = express();
+const port = 8800;
+const mongoose = require('mongoose');
+const cors=require('cors');
+const bodyparser=require('body-parser')
+
+app.use(cors());
+app.use(bodyparser.urlencoded({extended:false}));
+app.use(bodyparser.json());
+
+// mongoose connection estblished
+mongoose.connect(uri,{useNewUrlParser:true,useUnifiedTopology:true})
+                    .then(() => console.log('Connected to MongoDB Successfully......'))
+                    .catch(err => console.log('Error occured while connecting MongoDB '+err));
+
+console.log("connect");
+
+// set path for router file
+app.use('/', require('./routes/api/userinfo'));
+ 
+// listening port
+app.listen(port, () => {
+console.log("Port 8800 running on browser...");
+});
