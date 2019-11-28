d48de1f2d759fcdb93a88dd0e100c4928ad1f095
Pratikshatippe <54614091+Pratikshatippe@users.noreply.github.com>
2019-11-28 11:12:33 +0530

    doctor model update

---
+++ server/model/Doctor.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+
+const doctorSchema = new mongoose.Schema({
+    Name:{
+        type:String,
+        required: true,
+    },
+    Designation:{
+        type:String,
+        required:true
+    },
+    Experience:{
+        type:String,
+        required:true
+    },
+    Degree:{
+        type:String,
+        required:true
+    },
+    CabinNO:{
+        type:String,
+        required:true
+    }
+});
+
+doctorSchema.set('collection','doctorInfo');
+
+module.exports = Doctor = mongoose.model('doctorInfo',doctorSchema);
