f5cf775bd69666fcb88d9191acf1b45d0e70c9a5
Pratikshatippe <54614091+Pratikshatippe@users.noreply.github.com>
2019-11-29 12:32:34 +0530

    register js updated in client side

--- client/src/component/Register/register.jsx
+++ client/src/component/Register/register.jsx
@@ -1,32 +1,78 @@
 import React, {Component} from 'react';
 import './register.css';
 import logo from '../../images/register.png';
+import axios from 'axios';
+// import Login from '../LoginPage/login';
+// import { Link, Redirect } from 'react-router-dom';
 
 
 export class Register extends Component{
+    constructor(){
+        super();
+        this.state = {
+            username:'',
+            password:'',
+            confirmPassword:'',
+            page:"currentpage"
+        }
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+    handleChange(e){
+        const {name,value} = e.target
+        this.setState({
+            [name]:value
+        });
+        // let confirmPassword = e.target.name;
+        // this.setState({ [confirmPassword]: e.target.value});
+    }
+    async handleSubmit(){
+        const { username, password, confirmPassword }=this.state;
+        try {
+            const responses = await axios.post('http://localhost:8800/register/userRegister', {
+             
+                auth:{
+                    username:this.state.username,
+                    password:this.state.password,
+                    confirmPassword:this.state.confirmPassword
+                }
+            }).then(store=>{
+                console.log(store);
+                alert("done");
+            }) 
+    }
+        
+            catch(err){
+                console.log(err);
+            }
+        }
+        
+    
+    
 render(){
-    return(
-        <div className="main-content">
-            <h1>Register</h1>
-            <div className="logo">
-                <img src={logo} alt={""} className="registerlogo"/>
+    // const { username, password, confirmPassword }=this.state;
+    
+    return (
+            <div className="main-content">
+                <h1>Register</h1>
+                <div className="logo">
+                    <img src={logo} alt={""} className="registerlogo"/>
+                </div>
+                    <div className="username">
+                        <input type="text" placeholder="username" name="username" className="userName" onChange={this.handleChange} />
+                    </div>
+                    <div className="password">
+                        <input type="password" placeholder="password" name="password" className="passWord" onChange={this.handleChange}/>
+                    </div>
+                    <div className="password">
+                        <input type="password" placeholder="Confirm-password" name="confirmPassword" className="confirmPassWord" onChange={this.handleChange}/>
+                    </div>
+                    <div className="login">
+                        <input type="submit" className="loginbtn" onClick={this.handleSubmit}/>
+                    </div>
             </div>
-            <form onSubmit={this.submitForm}>
-                <div className="username">
-                    <input type="text" placeholder="username" name="username" className="userName" />
-                </div>
-                <div className="password">
-                    <input type="password" placeholder="password" name="password" className="passWord"/>
-                </div>
-                <div className="password">
-                    <input type="password" placeholder="Confirm-password" name="password" className="confirmPassWord"/>
-                </div>
-                <div className="login">
-                    <input type="submit" className="loginbtn"/>
-                </div>
-            </form>
-        </div>
     )
+    
 }
 }
 
